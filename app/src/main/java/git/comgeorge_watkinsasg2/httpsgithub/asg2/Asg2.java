7ef1cf4634b943aabdcadd2b1bea25a0cc821e88
George-Watkins <george.watkins.13@cnu.edu>
2016-02-19 22:47:21 -0500

    Camera Button now works -> will open camera, allow picture to be taken and then import the picture into the app as imageView.  Make sure that it fits the screen of different sized devices!! Reset Button works -> All it does is makes the image invisible and then allows the user to see the camera button again and take another picture.  Does not ask the user if they want to reset or not before making imageView invisible

--- app/src/main/java/git/comgeorge_watkinsasg2/httpsgithub/asg2/Asg2.java
+++ app/src/main/java/git/comgeorge_watkinsasg2/httpsgithub/asg2/Asg2.java
@@ -2,17 +2,36 @@ package git.comgeorge_watkinsasg2.httpsgithub.asg2;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 public class Asg2 extends AppCompatActivity {
+
+    ImageView newPicture;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +40,16 @@ public class Asg2 extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        newPicture = (ImageView)findViewById(R.id.newPicture);
+        ImageView cameraButton = (ImageView)findViewById(R.id.cameraButton);
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,0);
+            }
+        });
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     @Override
@@ -37,24 +66,24 @@ public class Asg2 extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        switch (id){
+        switch (id) {
             case R.id.settings:
-                Toast.makeText(this,"settings goes here", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "settings goes here", Toast.LENGTH_SHORT).show();
                 return true;
             case R.id.reset:
-                Toast.makeText(this,"reset goes here", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "reset goes here", Toast.LENGTH_SHORT).show();
                 return true;
             case R.id.share:
-                Toast.makeText(this,"share goes here", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "share goes here", Toast.LENGTH_SHORT).show();
                 return true;
             case R.id.about:
                 doHelp();
                 return true;
             case R.id.action_edit:
-                Toast.makeText(this,"edit goes here", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "edit goes here", Toast.LENGTH_SHORT).show();
                 return true;
             case R.id.action_colorize:
-                Toast.makeText(this,"colorize the picure here", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "colorize the picure here", Toast.LENGTH_SHORT).show();
                 return true;
         }
 
@@ -73,4 +102,59 @@ public class Asg2 extends AppCompatActivity {
         AlertDialog dialog = builder.create();
         dialog.show();
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Asg2 Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://git.comgeorge_watkinsasg2.httpsgithub.asg2/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    public void doReset(MenuItem item) {
+        newPicture.setVisibility(View.INVISIBLE);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Asg2 Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+//                 TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://git.comgeorge_watkinsasg2.httpsgithub.asg2/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
+
+    //makes the captured image show up on the screen after picture is taken
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        newPicture.setVisibility(View.VISIBLE);
+        if(requestCode == 0) {
+            Bitmap theImage = (Bitmap) data.getExtras().get("data");
+            newPicture.setImageBitmap(theImage);
+        }
+    }
+
+//    }
 }
